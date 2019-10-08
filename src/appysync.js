8afac2a1ef17e530e89d98a8bb1f3aeccdbc53a4
midimurphdesigns <midimurphdesigns@gmail.com>
2019-10-08 16:44:20 -0700

    first commit

---
+++ src/appysync.js
@@ -0,0 +1,16 @@
+﻿{
+    "UserAgent": "aws-amplify-cli/0.1.0",
+    "Version": "1.0",
+    "IdentityManager": {
+        "Default": {}
+    },
+    "AppSync": {
+        "Default": {
+            "ApiUrl": "https://bffx33rswre43hjbvptnnsysva.appsync-api.us-west-2.amazonaws.com/graphql",
+            "Region": "us-west-2",
+            "AuthMode": "API_KEY",
+            "ApiKey": "da2-oru2yhkmjbfvjoeicofwlmgn2a",
+            "ClientDatabasePrefix": "Recipes API_API_KEY"
+        }
+    }
+}
